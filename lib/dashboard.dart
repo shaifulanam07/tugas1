c4a9cf509690c9f3d13cbbce1c3c9217f345a40d
shaifulanam07 <nekobrentb@gmail.com>
2024-10-23 17:30:27 +0700

    memperbarui lib

---
+++ lib/dashboard.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+        leading: SizedBox(),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to the Dashboard Bro!',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); // Kembali ke halaman login
+              },
+              child: Text('Logout'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
